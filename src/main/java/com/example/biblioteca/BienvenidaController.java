ac6d14e11d4defca2e4bbbf7cce5c3881062e3df
PabloGMOEducastur <pablogmo52@educastur.es>
2025-10-24 19:44:57 +0200

    Cambiado pom.xml a version correcta para que funcione BienvenidaController

--- src/main/java/com/example/biblioteca/BienvenidaController.java
+++ src/main/java/com/example/biblioteca/BienvenidaController.java
@@ -1,8 +1,20 @@
 package com.example.biblioteca;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class BienvenidaController {
 
-    public static void main(String[] args) {
-        System.out.println("Bienvenido a Monte Naranco");
+    @GetMapping
+    public String bienvenida () {
+        return "Bienvenido IES Monte Naranco";
     }
+
+
+
+
 }
+
